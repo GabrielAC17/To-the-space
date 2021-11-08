cc5a085eccd3c7a14bf27dbb628c13b50df2c639
Gabriel de Andrade Cordeiro <gabriel.cordeiro@chamatheapp.com>
2021-11-07 22:18:02 -0300

    Initial Commit

---
+++ To the space/To_the_spaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_the_spaceApp.swift
+//  To the space
+//
+//  Created by Chama User on 07/11/21.
+//
+
+import SwiftUI
+
+@main
+struct To_the_spaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
